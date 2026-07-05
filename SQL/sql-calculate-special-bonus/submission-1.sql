3b87c9bb30f4ccd494201efe7b05b09e3b9e92a4
Patasri santra <patasrisantra971@gmail.com>
2026-07-05 22:16:20 +0530

    Add: sql-calculate-special-bonus - submission-1

---
+++ SQL/sql-calculate-special-bonus/submission-1.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+SELECT employee_id, 
+       CASE
+          WHEN employee_id%2 = 0 OR name LIKE 'M%' THEN 0
+          ELSE salary
+        END AS bonus
+FROM employees
+ORDER BY employee_id ASC;
